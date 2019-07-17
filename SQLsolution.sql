b6f27fd0599f70d05e54b50c5d614723a8771374
sumitJRVS <51926855+sumitJRVS@users.noreply.github.com>
2019-07-17 09:28:27 -0400

    Update SQLsolution.sql

--- SQLsolution.sql
+++ SQLsolution.sql
@@ -1,4 +1,4 @@
----A-1
+--- Query for Solution#1
 
 
 SELECT cpu_number,host_id, total_mem,
@@ -6,7 +6,7 @@ FROM host_info
 GROUP BY host_id
 ORDER BY total_mem;
 
----A-2---
+--- Query for Solution#2
 
 SELECT host_info.id, host_info.hostname, host_info.total_mem, avg(host_info.total_mem - host_usage.memory_free) as Used Mem, (date_trunc('hour', host_usage."timestamp") + date_part('minute',host_usage."timestamp")::int / 5 * interval '5 min') as five_min_interval
 FROM	host_info 
